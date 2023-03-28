99af660c8d301e53f7b49cb4c7aca5fa8babbdf8
Brandon Lozano <brandon.lozano@benilde.edu.ph>
2023-03-28 18:24:04 +0800

    commit

---
+++ pages/portfolio/index.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/Layout"
+
+export default function index() {
+  return (
+    <Layout>
+      <div className="ml-auto mr-auto mt-auto">TO BE FILLED</div>
+    </Layout>
+  )
+}
